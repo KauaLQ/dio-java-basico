c552f2ed082ad5595094b7daecae22e3797df686
KauaLQ <limakaua610@gmail.com>
2024-08-10 01:53:22 -0300

    Aula sobre tipos de variáveis.

--- TiposDeVariaveis/src/TiposVariaveis.java
+++ TiposDeVariaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        //Variáveis primitivas
+        byte idade = 43;
+        short ano = 2024;
+        int cep = 62680000;
+        long cpf = 98077842279L;
+        float pi = 3.14F;
+        double salario = 1423.17;
+        boolean entrada = true;
+        char bits = 127;
+
+        //Representação de texto
+        String nome = "Kauã";
+
+        //Valores Constantes
+        final double VALOR_DE_PI = 3.14;
     }
 }
