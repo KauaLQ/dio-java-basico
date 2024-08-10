97512b2d425a60baf17699f2d840996738df2762
KauaLQ <limakaua610@gmail.com>
2024-08-10 08:04:21 -0300

    Exemplo SmarTv alterações básicas.

--- ConceitosJava/src/edu/kaua/exemplo/Usuario.java
+++ ConceitosJava/src/edu/kaua/exemplo/Usuario.java
@@ -6,14 +6,14 @@ public class Usuario {
     public static void main(String[] args) {
         SmartTv smartTv = new SmartTv();
 
-        System.out.print("Estado da TV: " + smartTv.estadoDaTv);
+        System.out.print("Estado da TV: " + smartTv.estadoDaTv + " | ");
 
         String primeiroValor = JOptionPane.showInputDialog("Digite algo...");
         if (primeiroValor.equals("Ligar")){
             smartTv.ligarTv();
-            System.out.print("Estado da TV: " + smartTv.estadoDaTv);
-            System.out.print("Canal da Tv: " + smartTv.canal);
-            System.out.print("Volume da TV: " + smartTv.volume);
+            System.out.print("Estado da TV: " + smartTv.estadoDaTv + " | ");
+            System.out.print("Canal da Tv: " + smartTv.canal + " | ");
+            System.out.print("Volume da TV: " + smartTv.volume + " | ");
         }
         else {
             System.out.print("Comando inválido");
